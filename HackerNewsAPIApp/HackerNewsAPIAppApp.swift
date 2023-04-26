9e67fd1a4c986dee4373296efe24b829d66474b7
Ian Slane <slaneian@gmail.com>
2023-04-26 15:54:55 -0600

    Initial Commit

---
+++ HackerNewsAPIApp/HackerNewsAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAPIAppApp.swift
+//  HackerNewsAPIApp
+//
+//  Created by Ian Slane on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
